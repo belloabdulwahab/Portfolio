cc0103531b07f216050abb904c31fb4f3075d0ed
Bello Abdulwahab <bello2005abdul@gmail.com>
2026-08-01 16:08:45 +0100

    Hero Image Update.

--- src/components/sections/Hero/Hero.jsx
+++ src/components/sections/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import { ArrowRight, Download } from "lucide-react";
 import "./Hero.css";
-import heroImage from "../../../assets/images/hero/hero-bg.jpeg";
+import heroImage from "../../../assets/images/hero/hero-bg5.jpg";
 
 function Hero() {
     return (
