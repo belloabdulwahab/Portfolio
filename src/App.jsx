28f186e50f21f46fe4ffe0c2e8ed6a1961f98583
Bello Abdulwahab <bello2005abdul@gmail.com>
2026-08-26 23:21:22 +0100

    temporary diagnostic change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import AppRouter from "./router/AppRouter";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <>
+    <AppRouter />
+
+    <div
+  style={{
+    position: "fixed",
+    bottom: 10,
+    left: 10,
+    zIndex: 99999,
+    background: "red",
+    color: "white",
+    padding: "8px",
+    fontSize: "12px",
+  }}
+>
+  Width: {window.innerWidth}
+</div>
+</>
+  );
 }
+
 
 export default App;
